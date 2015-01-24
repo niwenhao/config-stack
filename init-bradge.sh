88f359af1070f80d39c476fb907327029719b402
niwenhao <niwenhao@gmail.com>
2015-01-24 10:13:40 +0900

    add bradge config

---
+++ init-bradge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+function init_br() {
+	local brname=$1
+	local braddr=$2
+
+	brctl show | grep $brname >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		brctl addbr $brname
+		ifconfig $brname $braddr netmask 255.255.255.0
+	fi
+}
+
+init_br brmgr 10.0.0.1
+init_br brtun 10.0.1.1
+init_br brstg 10.0.2.1
+init_br brext 10.0.3.1
